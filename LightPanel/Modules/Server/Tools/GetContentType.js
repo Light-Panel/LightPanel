c9485dda027745bbff69ad510fa96f242d209b85
LmanTW <82919705+LmanTW@users.noreply.github.com>
2023-11-05 15:11:35 +0800

    Delete LightPanel directory

--- LightPanel/Modules/Server/Tools/GetContentType.js
+++
@@ -1,8 +0,0 @@
-//Get Content Type From File Extension
-module.exports = (extension) => {
-  if (extension === '.css') return 'text/css'
-  else if (extension === '.js') return 'text/javascript'
-  else if (extension === '.json') return 'application/json'
-  else if (extension === '.svg') return 'image/svg+xml'
-  else return 'text/plan'
-}
