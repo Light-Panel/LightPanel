22a023f4539045fcbf265ff012f56344bf429944
LmanTW <82919705+LmanTW@users.noreply.github.com>
2023-11-06 17:17:54 +0800

    Update Install.js

--- Install.js
+++ Install.js
@@ -1,18 +1,23 @@
 const os = require('os')
 const fs = require('fs')
 
-if (['linux', 'win32', 'darwin'].includes(os.platform())) {
+let pathSymbol
+if (os.platform() === 'linux') pathSymbol = '/'
+else if (os.platform() === 'win32') pathSymbol = '\\'
+
+if (['linux', 'win32'].includes(os.platform())) {
   (async () => {
-    console.log('Installing Light Panel')
+    log(`Installing Light Panel (${getPath(process.cwd(), ['LightPanel-Data'])})`)
 
     let files = {}
+    let fileAmount = 0
   
     await getDirectory(files, 'https://api.github.com/repos/Light-Panel/LightPanel/contents/LightPanel')
   
     async function getDirectory (parent, url) {
       let response = await (await fetch(url)).json()
 
-      if (response.message.includes("API rate limit exceeded"))
+      if (response.message !== undefined && response.message.includes("API rate limit exceeded")) throw new Error('Github API Rate Limit (Please Try Again Later)')
   
       for (let item of response) {
         if (item.type === 'file') parent[item.name] = item.url
@@ -20,28 +25,35 @@ if (['linux', 'win32', 'darwin'].includes(os.platform())) {
           parent[item.name] = {}
           await getDirectory(parent[item.name], item.url)
         }
+
+        fileAmount++
       }
     }
   
     let installPath = getPath(process.cwd(), ['LightPanel-Data'])
-    if (!fs.existsSync(installPath)) fs.mkdirSync(installPath)
+    if (fs.existsSync(installPath)) fs.rmdirSync(installPath, { recursive: true })
+    fs.mkdirSync(installPath)
+    
+    let downloadedFileAmount = 0
 
-    downloadDirectory(files, installPath)
+    await downloadDirectory(files, installPath)
   
     async function downloadDirectory (folder, path) {
       for (let item of Object.keys(folder)) {
         if (typeof folder[item] === 'string') {
-          log(`Downloading ${item}`)
+          log(`${progress((100/fileAmount)*downloadedFileAmount)} Downloading ${item}`)
 
           fs.writeFileSync(getPath(path, [item]), Buffer.from(await (await fetch(folder[item])).arrayBuffer()))
         } else {
-          log(`Creating Directory ${item}`)
-
           fs.mkdirSync(getPath(path, [item]))
           await downloadDirectory(folder[item], getPath(path, [item]))
         }
+
+        downloadedFileAmount++
       }
     }
+
+    log(`Light Panel Installed (${getPath(process.cwd(), ['LightPanel-Data'])})\n`)
   })()
 } else throw new Error(`Unsupported Platform: [${os.platform()}]`)
 
@@ -50,6 +62,16 @@ function log (string) {
   process.stdout.clearLine()
   process.stdout.cursorTo(0)
   process.stdout.write(string)
+}
+
+//Display Progress
+function progress (value) {
+  let string = ''
+
+  for (let i = 0; i < value/10; i++) string+='|'
+  while (string.length < 10) string+='-'
+
+  return `[ ${string} ]`
 }
 
 //Get Absolute Path
@@ -63,7 +85,3 @@ function getPath (basePath, move) {
 
   return analysis.join(pathSymbol)
 }
-
-let pathSymbol
-if (os.platform() === 'linux') pathSymbol = '/'
-else if (os.platform() === 'win32') pathSymbol = '\\'
